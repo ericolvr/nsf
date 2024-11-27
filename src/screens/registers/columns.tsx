3e46c05427587d0547c3475388d1281cf78dedeb
ericolvr@gmail.com <ericolvr@gmail.com>
2024-11-27 16:13:19 -0300

    delete function

--- src/screens/registers/columns.tsx
+++ src/screens/registers/columns.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import { ColumnDef } from '@tanstack/react-table'
-import { ArrowUpDown, MapPin, Pen } from 'lucide-react'
-import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
+import { ArrowUpDown, Pen, Trash2 } from 'lucide-react'
+import { Button } from '@/components/ui/button'
+import ApiRegister from './service'
 
 
 export type Register = {
@@ -15,11 +16,23 @@ export type Register = {
     register_type: string
 }
 
-
 const typeMap = {
     0: 'Leitura',
     1: 'Escrita'
 }
+
+
+const deleteRegister = async (id: string, address: string, setRegister: React.Dispatch<React.SetStateAction<Register[]>>) => {
+
+    if (window.confirm(`Confirma a exclusão do Registrador ${address}?`)) {
+        try {
+            await ApiRegister.Delete(id)
+            setRegister(prevRegisters => prevRegisters.filter(address => address.id !== id));  // Update state
+        } catch (error) {
+            console.error("Error deleting serial:", error);
+        }
+    }
+};
 
 
 export const columns = (setRegister: React.Dispatch<React.SetStateAction<Register[]>>): ColumnDef<Register>[] => [
@@ -98,12 +111,18 @@ export const columns = (setRegister: React.Dispatch<React.SetStateAction<Registe
     {
         id: 'actions',
         cell: ({ row }) => {
+            const line = row.original
             return (
                 <div className='flex justify-end'>
                     <Link to={`/registers/edit/${row.original.id}`}
                         className='items-center justify-center p-3 rounded-md bg-white hover:bg-[#23CFCE] dark:bg-[#212121] dark:hover:bg-[#23CFCE] group'>
                         <Pen className='text-black dark:text-white dark:group-hover:text-black' strokeWidth={2} style={{ 'width': 19, 'height': 19}} />
                     </Link>
+                    <Trash2 
+                        onClick={() => deleteRegister(line.id, line.address, setRegister)} 
+                        strokeWidth={1.75} 
+                        className='h-5 w-5 ml-6 mt-[10px] text-black hover:text-gray-800 cursor-pointer'     
+                    />
                 </div>
             )
         }
