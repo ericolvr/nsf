a06ed23236cfa2e1d58025d9049465c2523466f7
ericolvr@gmail.com <ericolvr@gmail.com>
2024-11-27 16:12:05 -0300

    bracn service api

---
+++ src/screens/branchs/service.ts
@@ -0,0 +1,60 @@
+import axios from 'axios'
+const BASE_URL = import .meta.env.VITE_API_URL
+
+
+class ApiBranch {
+    static async GetBranchsByClients({ client }) {
+        try {
+            const response = await axios.get(
+                `${BASE_URL}/branchs/client/?client=${client}`
+            )
+            if (response.status === 200) {
+                return response.data
+            }
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static async Insert({ data }) {
+        try {
+            const response = await axios.post(
+                `${BASE_URL}/branchs/`,
+                data
+            )
+            if (response.status === 201) {
+                return response.status
+            }
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static async GetBranchByID({ id }) {
+        try {
+            const response = await axios.get(
+                `${BASE_URL}/branchs/id?id=${id}`
+                
+            )
+            if (response.status === 200) {
+                return response.data
+            }
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+    static async Update({ id, data }) {
+        try {
+            const response = await axios.patch(`${BASE_URL}/branchs/update/${id}`, data)
+            if (response.status === 200) {
+                return response.status
+            }
+            return response.data
+        } catch (error) {
+            console.log(error)
+        }
+    }
+}
+
+export default ApiBranch
