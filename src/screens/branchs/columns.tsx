3c3294f4feb96d266d577d99101d89bcc82da3bc
ericolvr@gmail.com <ericolvr@gmail.com>
2024-11-27 16:45:36 -0300

    delete branch

--- src/screens/branchs/columns.tsx
+++ src/screens/branchs/columns.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import { ColumnDef } from '@tanstack/react-table'
-import { ArrowUpDown, MapPin, Pen } from 'lucide-react'
+import { ArrowUpDown, Pen, Trash2 } from 'lucide-react'
 import { Button } from '@/components/ui/button'
 import { Link } from 'react-router-dom'
-
+import ApiBranch from './service'
 
 export type Branch = {
     id: string
@@ -11,6 +11,18 @@ export type Branch = {
     uniorg: string
 }
 
+
+const deleteBranch = async (id: string, name: string, setBranch: React.Dispatch<React.SetStateAction<Branch[]>>) => {
+
+    if (window.confirm(`Confirma a exclusão do Branch ${name}?`)) {
+        try {
+            await ApiBranch.Delete(id)
+            setBranch(prevBranchs => prevBranchs.filter(name => name.id !== id))
+        } catch (error) {
+            console.error("Error deleting branch:", error)
+        }
+    }
+}
 
 export const columns = (setBranch: React.Dispatch<React.SetStateAction<Branch[]>>): ColumnDef<Branch>[] => [
     {
@@ -49,12 +61,18 @@ export const columns = (setBranch: React.Dispatch<React.SetStateAction<Branch[]>
     {
         id: 'actions',
         cell: ({ row }) => {
+            const line = row.original
             return (
                 <div className='flex justify-end'>
                     <Link to={`/branchs/edit/${row.original.id}`}
                         className='items-center justify-center p-3 rounded-md bg-white hover:bg-[#23CFCE] dark:bg-[#212121] dark:hover:bg-[#23CFCE] group'>
                         <Pen className='text-black dark:text-white dark:group-hover:text-black' strokeWidth={2} style={{ 'width': 19, 'height': 19}} />
                     </Link>
+                    <Trash2 
+                        onClick={() => deleteBranch(line.id, line.name, setBranch)} 
+                        strokeWidth={1.75} 
+                        className='h-5 w-5 ml-6 mt-[10px] text-black hover:text-gray-800 cursor-pointer'     
+                    />
                 </div>
             )
         }
